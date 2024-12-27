56f2d3264f861df3cc2d1ee0a818be1599964a14
tonii <fatonimurfids@gmail.com>
2024-12-27 13:51:04 +0700

    fix responsive and hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,14 @@ import { Link } from 'react-scroll';
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  // Fungsi untuk menutup menu setelah link diklik
-  const handleLinkClick = () => setIsOpen(false);
-
   return (
     <nav className="fixed top-0 w-full bg-dracula-background shadow-lg z-50">
       <div className="container mx-auto p-4 flex justify-between items-center">
         {/* Logo */}
         <h1
-          className="font-bold typing-effect text-sm md:text-lg lg:text-xl" /* Ukuran berubah sesuai layar */
+          className="font-bold typing-effect 
+          text-left text-sm md:text-lg lg:text-xl" /* Responsif */
+          style={{ paddingLeft: '1rem' }} /* Tambah padding kiri untuk memepetkan */
         >
           <span className="text-dracula-keyword">from </span>
           <span className="text-white toni">toni </span>
@@ -53,7 +52,7 @@ export const Navbar = () => {
                 to={section}
                 smooth={true}
                 duration={500}
-                onClick={handleLinkClick} // Menutup menu setelah link diklik
+                onClick={() => setIsOpen(false)} // Menutup menu setelah link diklik
                 className="block py-2 md:py-0 text-dracula-text hover:text-dracula-string"
               >
                 {section.charAt(0).toUpperCase() + section.slice(1)}
